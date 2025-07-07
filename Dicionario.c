4b47e7c2063402351f664d695a6d78c4759c3534
Francisco Daniel Costa de Souza <ds9324621@gmail.com>
2025-07-07 20:11:35 -0300

    feat: adição de estruturas básicas, structs para as palavras e seus significados.

---
+++ Dicionario.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+
+
+typedef struct significado{
+    char* texto;
+    struct significado* proximo;
+} significado;
+
+// struct para os significados de cada palavra.
+
+typedef struct palavra {
+    char* palavra;
+    significado* significados;
+    struct palavra* proxima;
+
+}palavra;
+
+// struct para as listas encadeadas de cada posição da hash,
+// contendo as palavras e seus significados.
+
+typedef struct dicionario {
+    palavra** baldes;
+
+     unsigned int tamanho_atual;
+    unsigned int numero_elementos;
+
+}dicionario;
+
+// struct para o dicionário, 
+//baldes é um array de ponteiros, onde cada ponteiro pode ser o
+//início de uma lista encadeada (para lidar com colisões).
+//Cada elemento dentro dessas listas encadeadas representa uma palavra e seus significados.
+int main(){
+
+
+
+
+    return 0;
+}
