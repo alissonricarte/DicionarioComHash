97420c3648d481e917e7f406a1e6a4c5aab3f2bb
Francisco Daniel Costa de Souza <ds9324621@gmail.com>
2025-07-13 14:21:31 -0300

    test: testes básicos com as funções que foram criadas

--- src/main.c
+++ src/main.c
@@ -9,11 +9,15 @@ int main(){
      dicionario* meu_dicionario = inicializar(4); 
 
     if (meu_dicionario != NULL) {
-        printf("Dicionario inicializado com sucesso!\n");
         
 
         inserir_palavra( meu_dicionario,"casa","lar");
          inserir_palavra( meu_dicionario,"casa","abrigo");
+         const char* palavra_a_buscar1 = "casa";
+          palavra* resultado_busca1 = buscar(meu_dicionario, palavra_a_buscar1);
+          printf("a palavra %s foi encontrada\n", resultado_busca1->palavra);
+
+         
     
     } else {
         printf("Falha ao inicializar o dicionario. Encerrando programa.\n");
