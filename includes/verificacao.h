0fc48567a2e02d99bd801f5f14bd33306c61045e
Francisco Daniel Costa de Souza <ds9324621@gmail.com>
2025-07-14 10:33:33 -0300

    feat: função para converter letras de maiusculas para minúsculas

--- includes/verificacao.h
+++ includes/verificacao.h
@@ -2,6 +2,11 @@
 
 #ifndef VERIFICACAO_H 
 #define VERIFICACAO_H 
+#include <stdbool.h> 
+#include <ctype.h>   
+#include <string.h>
+
+ void covert_maisculas_minusculas(char* str);
 
 
 #endif 
--- src/verificacao.c
+++ src/verificacao.c
@@ -1,1 +1,10 @@
 #include "../includes/verificacao.h"
+#include<stdio.h>
+#include<ctype.h>
+
+void covert_maisculas_minusculas(char* str){
+
+    for(int i = 0; str[i] != '\0'; i++){
+        str[i] = tolower ((unsigned char )str[i]);
+    }
+}
