70dae303f1a224afbd0806cde882b4471cf9f545
Francisco Daniel Costa de Souza <ds9324621@gmail.com>
2025-07-15 00:31:20 -0300

    refactor

--- includes/dicionario.h
+++ includes/dicionario.h
@@ -3,18 +3,35 @@
 #ifndef DICIONARIO_H
 #define DICIONARIO_H
 
-typedef struct significado significado;
+
+typedef struct significado
+{
+    char *texto;
+    struct significado *proximo;
+} significado;
 // struct para os significados de cada palavra.
 
-typedef struct palavra palavra;
+typedef struct palavra
+{
+    char *palavra;
+    significado *significados;
+    struct palavra *proxima;
+} palavra;
 // struct para as listas encadeadas de cada posição da hash,
 // contendo as palavras e seus significados.
 
-typedef struct dicionario dicionario;
+
+typedef struct dicionario
+{
+    palavra **baldes;
+    unsigned int tamanho_atual;
+    unsigned int numero_elementos;
+} dicionario;
 // struct para o dicionário,
 // baldes é um array de ponteiros, onde cada ponteiro pode ser o
 // início de uma lista encadeada (para lidar com colisões).
 // Cada elemento dentro dessas listas encadeadas representa uma palavra e seus significados.
+
 
 dicionario *inicializar(unsigned int tam_inicial);
 
